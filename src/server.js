025ec7a75d4514e115187b082fad72cd030d4c05
Alexey Ermakov <strangebrew@yandex.ru>
2022-02-27 14:54:10 +0300

    Handle messages

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import { v4 as uuid } from "uuid";
 import { WebSocketServer } from "ws";
 
 const clients = {};
+const messages = [];
 
 const wss = new WebSocketServer({ port: 8000 });
 
@@ -11,8 +12,22 @@ wss.on("connection", (ws) => {
 
 	console.log("new client", id);
 
+	ws.send(JSON.stringify(messages));
+
 	ws.on("message", (rawMessage) => {
-		console.log("raw message", rawMessage);
+		console.log("raw message", JSON.parse(rawMessage.toString()));
+		try {
+			const { name, message } = JSON.parse(rawMessage.toString());
+			console.log("received", name, message);
+
+			messages.push({ name, message });
+
+			for (const id in clients) {
+				clients[id].send(JSON.stringify([{ name, message }]));
+			}
+		} catch (e) {
+			console.error(e.message);
+		}
 	});
 
 	ws.on("close", () => {
